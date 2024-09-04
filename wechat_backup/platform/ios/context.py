27b10f91ef33b44fb3f3cc3c2b081f7a8536563d
Garrett HE <garrett.he@outlook.com>
2024-09-04 11:05:27 +0800

    Added functions of context for platform ios

--- wechat_backup/platform/ios/context.py
+++ wechat_backup/platform/ios/context.py
@@ -14,4 +14,25 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import wechat_backup.platform.ios.context
+import sqlite3
+from dataclasses import dataclass
+from wechat_backup.context import WechatPlatform, WechatContext
+from wechat_backup.helper import sqlite_connect
+
+
+@dataclass
+class WechatContextIos(WechatContext):
+    doc_dir: str
+    contact_db: sqlite3.Connection
+    mm_db: sqlite3.Connection
+
+
+def new_context(config: dict):
+    return WechatContextIos(
+        platform=WechatPlatform.iOS,
+        user_id=config['user_id'],
+        doc_dir=config['doc_dir'],
+        mm_db=sqlite_connect('%s/DB/MM.sqlite' % config['doc_dir']),
+        contact_db=sqlite_connect('%s/DB/WCDB_Contact.sqlite' % config['doc_dir']),
+        emoji_cache=config['emoji_cache']
+    )
